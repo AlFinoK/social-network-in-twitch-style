ac3ee39f3a48bd3c6d28fdbfa32016fe3059647c
Alfino <nikitamalashkin73@gmail.com>
2024-06-22 21:04:22 +0500

    add sidebar + routes and some changes

--- src/app/common-ui/profile-card/profile-card.component.ts
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -1,13 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { IProfile } from '../../data/interfaces/profile.interface';
+import { ImgUrlPipe } from '../../helpers/pipes/img-url.pipe';
 
 @Component({
   selector: 'app-profile-card',
   standalone: true,
-  imports: [],
+  imports: [ImgUrlPipe],
   templateUrl: './profile-card.component.html',
-  styleUrl: './profile-card.component.scss',
+  styleUrls: ['./profile-card.component.scss'],
 })
 export class ProfileCardComponent {
+  firstWordUppercase(word: string): string {
+    if (typeof word === 'string' && word.length > 0) {
+      return word[0].toUpperCase() + word.slice(1);
+    }
+    return '';
+  }
+
   @Input() profile!: IProfile;
 }
